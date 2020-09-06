9ba37c9b6dbc13fb2b8b946c7759d770d3109d2b
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-06 16:26:18 -0500

    Creacion del Componente Home

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { ProjectsService } from 'src/app/services/projects.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor(public projectService:ProjectsService) { }
+
+  ngOnInit(): void {
+  }
+
+}
