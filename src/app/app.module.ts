d850eb2592a2a68fb543994b2277b72c0696ecf0
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-06 16:28:51 -0500

    Adicion de los Componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitComponent } from './components/git/git.component';
+import { HomeComponent } from './components/home/home.component';
+import { SocialNetworkComponent } from './components/social-network/social-network.component';
+import { LateralMenuComponent } from './components/lateral-menu/lateral-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitComponent,
+    HomeComponent,
+    SocialNetworkComponent,
+    LateralMenuComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: []
 })
 export class AppModule { }
