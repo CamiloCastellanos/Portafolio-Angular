ac3735eddabab6c5aa9e5c4a90c550c207d5f243
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-06 16:28:02 -0500

    Creacion dell model para Proyectos

---
+++ src/app/models/project.model.ts
@@ -0,0 +1,16 @@
+export class Project {
+  titulo: string;
+  lenguaje: string;
+  descripcion: string;
+  imagen: string;
+  url: string;
+
+  constructor() {
+    this.titulo = "";
+    this.lenguaje = "";
+    this.descripcion = "";
+    this.imagen = "";
+    this.url;
+  }
+
+}
