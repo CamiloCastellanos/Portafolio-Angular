c54462481c9aef84cd950d91d3e5fe4678b02822
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-06 18:14:07 -0500

    Estructura Basica de la vista de los Proyectos

--- src/app/components/git/git.component.ts
+++ src/app/components/git/git.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/project.model';
+import { ProjectsService } from 'src/app/services/projects.service';
 
 @Component({
   selector: 'app-git',
@@ -7,7 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GitComponent implements OnInit {
 
-  constructor() { }
+  listaProyecto: Project[] = []
+
+  constructor() {
+    let servicioProyecto = new ProjectsService();
+    this.listaProyecto = servicioProyecto.listaProyecto;
+  }
 
   ngOnInit(): void {
   }
--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,20 +1,66 @@
 import { Injectable } from '@angular/core';
-import {ProjectJson} from '../../assets/json/project.json';
-import { loadavg } from 'os';
-import { resolve } from 'dns';
-import { rejects } from 'assert';
-import { throwError } from 'rxjs';
+import { Project } from '../models/project.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectsService {
 
-  constructor() {
+  listaProyecto: Project[] = []
 
+  constructor() {
+    this.cargarProyectos();
   }
 
-  cargarProjectos(){
+  cargarProyectos() {
+    let proyectosJSON = {
+      "proyectos": [{
+        "titulo": "SPA",
+        "lenguaje": "sin_imagen",
+        "descripcion": "Una aplicación de una sola página. En la cual se busca a unos super Heroes",
+        "imagen": "sin_imagen.png",
+        "url": "https://github.com/CamiloCastellanos/Angular/tree/master/2-Proyecto"
+      },
+      {
+        "titulo": "SPA",
+        "lenguaje": "sin_imagen",
+        "descripcion": "Una aplicación de una sola página. En la cual se busca a unos super Heroes",
+        "imagen": "sin_imagen.png",
+        "url": "https://github.com/CamiloCastellanos/Angular/tree/master/2-Proyecto"
+      },
+      {
+        "titulo": "SPA",
+        "lenguaje": "sin_imagen",
+        "descripcion": "Una aplicación de una sola página. En la cual se busca a unos super Heroes",
+        "imagen": "sin_imagen.png",
+        "url": "https://github.com/CamiloCastellanos/Angular/tree/master/2-Proyecto"
+      },
+      {
+        "titulo": "SPA",
+        "lenguaje": "sin_imagen",
+        "descripcion": "Una aplicación de una sola página. En la cual se busca a unos super Heroes",
+        "imagen": "sin_imagen.png",
+        "url": "https://github.com/CamiloCastellanos/Angular/tree/master/2-Proyecto"
+      },
+      {
+        "titulo": "SPA",
+        "lenguaje": "Angular",
+        "descripcion": "Una aplicación de una sola página. En la cual se busca a unos super Heroes",
+        "imagen": "sin_imagen.png",
+        "url": "https://github.com/CamiloCastellanos/Angular/tree/master/2-Proyecto"
+      }
+      ]
+    }
+
+    for (const item in proyectosJSON.proyectos) {
+      let proyecto = new Project();
+      proyecto.titulo = proyectosJSON.proyectos[item].titulo;
+      proyecto.lenguaje = proyectosJSON.proyectos[item].lenguaje;
+      proyecto.descripcion = proyectosJSON.proyectos[item].descripcion;
+      proyecto.imagen = proyectosJSON.proyectos[item].imagen;
+      proyecto.url = proyectosJSON.proyectos[item].url;
+      this.listaProyecto.push(proyecto);
+    }
 
   }
 }
