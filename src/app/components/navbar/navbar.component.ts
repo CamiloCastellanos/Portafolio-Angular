f42ba0c5d90b5cea11fab836013baa2f415c3eb1
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-14 20:32:39 -0500

    Estilo de NavBar con menu superior

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -12,4 +12,16 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  desplegar(): void {
+    var burger = document.querySelector('.burger');
+    var menu = document.querySelector('#navBarMenu');
+    burger.classList.toggle('is-active');
+    menu.classList.toggle('is-active');
+  }
+
+  menuLateral(): void {
+    document.getElementById("menuLateral").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+
 }
