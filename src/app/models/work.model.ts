0e15f0dd38badd4c2dc53c54fbaaa204dcf5f764
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-09-16 17:19:26 -0500

    Creacion modelo "Work"

---
+++ src/app/models/work.model.ts
@@ -0,0 +1,14 @@
+export class Work {
+  titulo: string;
+  descripcion: string;
+  imagen: string;
+  url: string;
+
+  constructor() {
+    this.titulo = "";
+    this.descripcion = "";
+    this.imagen = "";
+    this.url;
+  }
+
+}
