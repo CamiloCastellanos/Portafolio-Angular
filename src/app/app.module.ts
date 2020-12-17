bedd2d6209131a03b4fd944e27def24ded425576
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2020-12-17 10:28:01 -0500

    Adicion de FireStore, ReactiveForms, Toastr y BrowserAnimations al proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
 /*Rutas*/
 import {APP_ROUTING} from './app.routes'
+import { Router } from '@angular/router';
 
-/*Componentes*/
+//Modulos
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { environment } from 'src/environments/environment';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+/*Componentes*/
 import { AppComponent } from './app.component';
 import { GitComponent } from './components/git/git.component';
 import { HomeComponent } from './components/home/home.component';
@@ -14,7 +21,6 @@ import { LateralMenuComponent } from './components/lateral-menu/lateral-menu.com
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { Router } from '@angular/router';
 import { WorkComponent } from './components/work/work.component';
 import { LearningComponent } from './components/learning/learning.component';
 
@@ -34,7 +40,12 @@ import { LearningComponent } from './components/learning/learning.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    APP_ROUTING
+    APP_ROUTING,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
