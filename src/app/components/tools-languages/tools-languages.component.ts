b9f0e5c9580e599c6dae465bcb7522547c492f9a
CamiloCastellanos <camilo.castellanos.puentes@gmail.com>
2021-04-10 14:44:41 -0500

    Adicion de Filtro para saber que mostrar de la lista de Herramientas

--- src/app/components/tools-languages/tools-languages.component.ts
+++ src/app/components/tools-languages/tools-languages.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output } from '@angular/core';
 import { ToolsLanguages } from 'src/app/models/ToolsLanguages.model';
 import { ToolsLanguagesService } from 'src/app/services/tools-languages.service';
 
@@ -11,12 +11,15 @@ export class ToolsLanguagesComponent implements OnInit {
 
   listaHerramientas: ToolsLanguages[] = []
 
-  constructor() {
-    let serviceTool: ToolsLanguagesService = new ToolsLanguagesService();
-    this.listaHerramientas = serviceTool.listaHerramientas;
-  }
+  @Input() tipoListaDefault: number;
+  @Input() tipoLista: number;
 
+  constructor(public serviceTool: ToolsLanguagesService) { }
+  /// Estado = 0 (Solo Trabajo)
+  /// Estado = 1 (Solo Proyectos Personales)
+  /// Estado = 2 (Aplica para las 2)
   ngOnInit(): void {
+    this.listaHerramientas = this.serviceTool.listaHerramientas.filter(h => h.estado == this.tipoListaDefault);
   }
 
 }
--- src/app/services/tools-languages.service.ts
+++ src/app/services/tools-languages.service.ts
@@ -6,75 +6,103 @@ import { ToolsLanguages } from '../models/ToolsLanguages.model';
 })
 export class ToolsLanguagesService {
 
-  listaHerramientas:ToolsLanguages[]=[];
+  listaHerramientas: ToolsLanguages[] = [];
 
   constructor() {
     this.cargarHerramientas();
   }
 
-cargarHerramientas(){
-  let learningJSON = {
-    "aprendiendo": [
-      {
-        "nombre": "Github",
-        "imagen": "https://i.postimg.cc/mry0D2wh/GitHub.png",
-        "estado":0
-      },
-      {
-        "nombre": "Angular",
-        "imagen": "https://i.postimg.cc/xjPsq2Nh/angular.png",
-        "estado":0
-      },
-      {
-        "nombre": "Colab",
-        "imagen": "https://i.postimg.cc/4yXBw8qW/colab.png",
-        "estado":0
-      },
-      {
-        "nombre": "C#",
-        "imagen": "https://i.postimg.cc/TYYtD4rw/Csharp.png",
-        "estado":0
-      },
-      {
-        "nombre": "Ionic",
-        "imagen": "https://i.postimg.cc/TY7CqJ9c/ionic.png",
-        "estado":0
-      },
-      {
-        "nombre": "MongoDB",
-        "imagen": "https://i.postimg.cc/7Z5XBJtr/mongo.png",
-        "estado":0
-      },
-      {
-        "nombre": "Node JS",
-        "imagen": "https://i.postimg.cc/7L7948yB/nodejs.png",
-        "estado":0
-      },
-      {
-        "nombre": "Python",
-        "imagen": "https://i.postimg.cc/FRVxfyz3/python.png",
-        "estado":0
-      },
-      {
-        "nombre": "Bulma CSS",
-        "imagen": "https://i.postimg.cc/T1QQsVXf/bulmacss.png",
-        "estado":0
-      }
-      // {
-      //   "nombre": "",
-      //   "imagen": "",
-      //   "estado":0
-      // }
-    ]
+  cargarHerramientas() {
+    let learningJSON = {
+      "aprendiendo": [
+        {
+          "nombre": ".NET",
+          "imagen": "https://i.postimg.cc/k4wcJqjN/net.png",
+          "estado": 2
+        },
+        {
+          "nombre": "C#",
+          "imagen": "https://i.postimg.cc/TYYtD4rw/Csharp.png",
+          "estado": 2
+        },
+        {
+          "nombre": "Github",
+          "imagen": "https://i.postimg.cc/mry0D2wh/GitHub.png",
+          "estado": 2
+        },
+        {
+          "nombre": "Angular",
+          "imagen": "https://i.postimg.cc/xjPsq2Nh/angular.png",
+          "estado": 2
+        },
+        {
+          "nombre": "SQL Server",
+          "imagen": "https://i.postimg.cc/3JpHwqT4/SQLServer.png",
+          "estado":2
+        },
+        {
+          "nombre": "Visual Studio",
+          "imagen": "https://i.postimg.cc/T1GyhD4B/Visual-Studio.png",
+          "estado":2
+        },
+        {
+          "nombre": "Visual Studio Code",
+          "imagen": "https://i.postimg.cc/Prk2HVr5/Visual-Studio-Code.png",
+          "estado":2
+        },
+        {
+          "nombre": "Power BI",
+          "imagen": "https://i.postimg.cc/Pf0jTjBn/PowerBi.png",
+          "estado":2
+        },
+        {
+          "nombre": "Oracle SQL",
+          "imagen": "https://i.postimg.cc/MH6jGBcX/Oracle-SQL.png",
+          "estado":0
+        },
+        {
+          "nombre": "Ionic",
+          "imagen": "https://i.postimg.cc/TY7CqJ9c/ionic.png",
+          "estado": 1
+        },
+        {
+          "nombre": "Python",
+          "imagen": "https://i.postimg.cc/FRVxfyz3/python.png",
+          "estado": 1
+        },
+        {
+          "nombre": "Colab",
+          "imagen": "https://i.postimg.cc/4yXBw8qW/colab.png",
+          "estado": 1
+        },
+        {
+          "nombre": "MongoDB",
+          "imagen": "https://i.postimg.cc/7Z5XBJtr/mongo.png",
+          "estado": 1
+        },
+        {
+          "nombre": "Node JS",
+          "imagen": "https://i.postimg.cc/7L7948yB/nodejs.png",
+          "estado": 1
+        },
+        {
+          "nombre": "Bulma CSS",
+          "imagen": "https://i.postimg.cc/T1QQsVXf/bulmacss.png",
+          "estado": 1
+        },
+        {
+          "nombre": "Postman",
+          "imagen": "https://i.postimg.cc/25v1CG5C/Postman.png",
+          "estado":2
+        }
+      ]
+    }
+    for (const item in learningJSON.aprendiendo) {
+      let herramienta = new ToolsLanguages();
+      herramienta.nombre = learningJSON.aprendiendo[item].nombre;
+      herramienta.imagen = learningJSON.aprendiendo[item].imagen;
+      herramienta.estado = learningJSON.aprendiendo[item].estado;
+      this.listaHerramientas.push(herramienta);
+    }
   }
-
-  for (const item in learningJSON.aprendiendo) {
-    let herramienta = new ToolsLanguages();
-    herramienta.nombre = learningJSON.aprendiendo[item].nombre;
-    herramienta.imagen = learningJSON.aprendiendo[item].imagen;
-    this.listaHerramientas.push(herramienta);
-  }
-}
-
-
 }
