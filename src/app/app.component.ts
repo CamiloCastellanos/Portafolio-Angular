bdf6d73ac7f2f20573cbfe4c4df0365a8d013fe8
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-04-09 20:40:31 -0500

    Adicion de Particulas de fondo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,89 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Portafolio-Angular';
+
+  id = "tsparticles";
+
+  particlesOptions = {
+    background: {
+      color: {
+        value: "#fff"
+      }
+    },
+    fullScreen: {
+      enable: true,
+      zIndex: -1
+    },
+    fpsLimit: 60,
+    interactivity: {
+      detectsOn: "canvas",
+      events: {
+        onClick: {
+          enable: true,
+          mode: "push"
+        },
+        onHover: {
+          enable: true,
+          mode: "repulse"
+        },
+        resize: true
+      },
+      modes: {
+        bubble: {
+          distance: 400,
+          duration: 2,
+          opacity: 0.8,
+          size: 40
+        },
+        push: {
+          quantity: 4
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4
+        }
+      }
+    },
+    particles: {
+      color: {
+        value: "#000"
+      },
+      links: {
+        color: "#000",
+        distance: 150,
+        enable: true,
+        opacity: 0.2,
+        width: 1
+      },
+      collisions: {
+        enable: true
+      },
+      move: {
+        direction: "none",
+        enable: true,
+        outMode: "bounce",
+        random: false,
+        speed: 1.5,
+        straight: false
+      },
+      number: {
+        density: {
+          enable: true,
+          value_area: 800
+        },
+        value: 80
+      },
+      opacity: {
+        value: 0.2
+      },
+      shape: {
+        type: "circle"
+      },
+      size: {
+        random: true,
+        value: 5
+      }
+    },
+    detectRetina: true
+  };
 }
