f2bbd12cc52213e8c78b41ae898757a1572123fc
CamiloCastellanos <camilo.castellanos.puentes@gmail.com>
2021-06-09 12:53:41 -0500

    Actualizacion Proyecto a Angular 11.2.14

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,4 +13,4 @@ const APP_ROUTES: Routes = [
     { path: '', pathMatch: 'full', redirectTo: 'home' }
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { relativeLinkResolution: 'legacy' });
