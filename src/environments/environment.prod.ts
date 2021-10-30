0b6af8225046805978424ca42821d41f2ef09945
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-10-30 18:17:28 -0500

    Actualizacion tamaño Titulos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: true,
   firebase : {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyCPo7JTRBbDoU2yIbhbTNREWACO7xlkMew",
+    authDomain: "portafolioangular-4f64f.firebaseapp.com",
+    databaseURL: "https://portafolioangular-4f64f.firebaseio.com",
+    projectId: "portafolioangular-4f64f",
+    storageBucket: "portafolioangular-4f64f.appspot.com",
+    messagingSenderId: "465578885884",
+    appId: "1:465578885884:web:ce4edca29843abe4940d79",
+    measurementId: "G-7C4MYXWBW9"
   }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase : {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "AIzaSyCPo7JTRBbDoU2yIbhbTNREWACO7xlkMew",
+    authDomain: "portafolioangular-4f64f.firebaseapp.com",
+    databaseURL: "https://portafolioangular-4f64f.firebaseio.com",
+    projectId: "portafolioangular-4f64f",
+    storageBucket: "portafolioangular-4f64f.appspot.com",
+    messagingSenderId: "465578885884",
+    appId: "1:465578885884:web:ce4edca29843abe4940d79",
+    measurementId: "G-7C4MYXWBW9"
   }
 };
 
