ec6f88a41de29661409a112b19a0133361fcb2a3
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2021-10-31 21:24:59 -0500

    Adicion e implementacion bulma-carousel

--- src/app/shared/components/tools-languages/tools-languages.component.ts
+++ src/app/shared/components/tools-languages/tools-languages.component.ts
@@ -1,13 +1,14 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
 import { ToolsLanguages } from '../../../models/ToolsLanguages.model';
 import { ToolsLanguagesService } from '../../../core/services/tools-languages.service';
+import bulmaCarousel from 'bulma-carousel/dist/js/bulma-carousel.min.js';
 
 @Component({
   selector: 'app-tools-languages',
   templateUrl: './tools-languages.component.html',
   styleUrls: ['./tools-languages.component.css']
 })
-export class ToolsLanguagesComponent implements OnInit {
+export class ToolsLanguagesComponent implements OnInit, AfterViewInit {
 
   listaHerramientas: ToolsLanguages[] = []
 
@@ -19,11 +20,26 @@ export class ToolsLanguagesComponent implements OnInit {
   /// Estado = 1 (Solo Proyectos Personales)
   /// Estado = 2 (Aplica para las 2)
   ngOnInit(): void {
-    this.listaHerramientas = this.serviceTool.listaHerramientas.filter(h =>
-                                                                            h.estado == this.tipoListaDefault
-                                                                            ||
-                                                                            h.estado == this.tipoLista
-                                                                      );
+    this.listaHerramientas = this.serviceTool.listaHerramientas.filter(
+      h =>
+        h.estado == this.tipoListaDefault
+        ||
+        h.estado == this.tipoLista
+    );
+    // this.carrucel();
   }
 
+  /**
+   * visualización carusel para los logos
+   */
+  ngAfterViewInit() {
+    bulmaCarousel.attach('#carousel', {
+      slidesToScroll: 1,
+      slidesToShow: 1,
+      autoplay: true,
+      loop: true,
+      pagination: true,
+      infinite: true
+    });
+  }
 }
