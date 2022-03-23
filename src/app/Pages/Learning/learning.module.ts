4c22db3d8821104d688a75ef7a40b59ddc1462b6
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2022-03-22 21:05:23 -0500

    Adicion de Linea de tiempo para pagina de seguir aprendiendo

--- src/app/Pages/Learning/learning.module.ts
+++ src/app/Pages/Learning/learning.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LearningComponent } from './components/learning/learning.component';
 import { LearningRoutingModule } from './learning.routes';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { MzdTimelineModule } from 'ngx-mzd-timeline';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { SharedModule } from 'src/app/shared/shared.module';
   imports: [
     CommonModule,
     LearningRoutingModule,
-    SharedModule
+    SharedModule,
+    MzdTimelineModule
   ]
 })
 export class LearningModule { }
