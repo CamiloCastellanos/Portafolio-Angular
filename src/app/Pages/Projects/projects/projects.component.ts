29cc99415f186faa3edd533ac05ce144e19e5b6a
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2023-12-11 19:56:25 -0500

    Se adicionan las cards para la visualización móvil en trabajos, además de ajustar el carousel

--- src/app/Pages/Projects/projects/projects.component.ts
+++ src/app/Pages/Projects/projects/projects.component.ts
@@ -12,6 +12,8 @@ import { ProjectsService } from '../services/projects.service';
 export class ProjectsComponent {
   projectList: Project[] = [];
   language: string = "es";
+  textButton: string = 'Ver más - Código - Demostración - Ver más - '
+  private textButtonLanguage = ['Ver más - Código - Demostración - Ver más - ', 'See more - Code - Demo - See more - ']
 
   constructor(private projectService: ProjectsService,
     private spinnerService: NgxSpinnerService,
@@ -19,12 +21,14 @@ export class ProjectsComponent {
     this.getProjects();
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       this.language = event.lang
+      this.textButton = event.lang == 'es' ? this.textButtonLanguage[0] : this.textButtonLanguage[1]
     });
   }
 
   ngOnInit(): void {
     this.spinnerService.show();
     this.language = this.translate.currentLang;
+    this.textButton = this.language == 'es' ? this.textButtonLanguage[0] : this.textButtonLanguage[1]
   }
 
   getProjects() {
--- src/app/Pages/Work/work.module.ts
+++ src/app/Pages/Work/work.module.ts
@@ -4,9 +4,10 @@ import { WorkRoutingModule } from './work.routes';
 import { TranslateModule } from '@ngx-translate/core';
 import { WorkComponent } from './work/work.component';
 import { CarouselComponent } from './components/carousel/carousel.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
-  declarations: [WorkComponent, CarouselComponent],
+  declarations: [WorkComponent, CarouselComponent, CardComponent],
   imports: [
     CommonModule,
     TranslateModule,
