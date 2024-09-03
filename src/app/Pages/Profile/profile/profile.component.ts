cc9cb7e66dd435469ab8e577bd9bd5a56b26cd38
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2024-09-02 22:09:45 -0500

    Se adiciona un about me en la pagina de perfil

--- src/app/Pages/Profile/profile/profile.component.ts
+++ src/app/Pages/Profile/profile/profile.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent { }
+export class ProfileComponent {
+  email = 'camilo.castellanos.puentes@gmail.com';
+  textTooltip = ""
+  lang: string = "es";
+
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+  }
+
+  copyEmail() {
+    navigator.clipboard.writeText(this.email);
+    this.textTooltip = "Copiado"
+    setTimeout(() => {
+      this.textTooltip = ""
+    }, 2500)
+  }
+}
