8e53cdbcd4d04eed0f21d552d70736bda4b8c987
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2025-02-09 21:20:14 -0500

    Se actualiza la distribución de los botones de las redes sociales para ser en formato horizontal cuando son dispositivos pequeños, además se ajusta la lógica para redireccionar a diferentes hojas de vida según el idioma

--- src/app/Pages/Profile/profile/profile.component.ts
+++ src/app/Pages/Profile/profile/profile.component.ts
@@ -8,14 +8,17 @@ import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 })
 export class ProfileComponent {
   email = 'camilo.castellanos.puentes@gmail.com';
-  textTooltip = ""
+  textTooltip = "";
   lang: string = "es";
-  typographyAbout="font-weight-500 typography-open-sans"
-  typographyTable="font-weight-500 typography-open-sans"
-  typographyFrontEndTitle="font-weight-600 typography-lora"
-  typographyFrontEnd="font-weight-500 typography-open-sans"
-  typographyBackEndTitle="font-weight-600 typography-lora"
-  typographyBackEnd="font-weight-500 typography-open-sans"
+  typographyAbout = "font-weight-500 typography-open-sans";
+  typographyTable = "font-weight-500 typography-open-sans";
+  typographyFrontEndTitle = "font-weight-600 typography-lora";
+  typographyFrontEnd = "font-weight-500 typography-open-sans";
+  typographyBackEndTitle = "font-weight-600 typography-lora";
+  typographyBackEnd = "font-weight-500 typography-open-sans";
+  curriculumVitaeEn = "https://drive.google.com/file/d/1uA8rPnOOOQmZM9FJQgI1m5DXGEt_MpN5/view?usp=drive_link";
+  curriculumVitaeEs = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
+
 
   constructor(private translate: TranslateService) {
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
