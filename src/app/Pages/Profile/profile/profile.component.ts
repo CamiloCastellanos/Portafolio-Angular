7f23959a603dbe606e9425f9c6bab17577526e62
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2025-05-29 18:19:26 -0500

    Se actualiza la pagina de “Perfil” en la cual se implementan sus nuevos componentes, además de cambiar la estética del mismo.

--- src/app/Pages/Profile/profile/profile.component.ts
+++ src/app/Pages/Profile/profile/profile.component.ts
@@ -7,30 +7,11 @@ import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
-  email = 'camilo.castellanos.puentes@gmail.com';
-  textTooltip = "";
   lang: string = "es";
-  typographyAbout = "font-weight-500 typography-open-sans";
-  typographyTable = "font-weight-500 typography-open-sans";
-  typographyFrontEndTitle = "font-weight-600 typography-lora";
-  typographyFrontEnd = "font-weight-500 typography-open-sans";
-  typographyBackEndTitle = "font-weight-600 typography-lora";
-  typographyBackEnd = "font-weight-500 typography-open-sans";
-  curriculumVitaeEn = "https://drive.google.com/file/d/1uA8rPnOOOQmZM9FJQgI1m5DXGEt_MpN5/view?usp=drive_link";
-  curriculumVitaeEs = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
-
 
   constructor(private translate: TranslateService) {
     this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
       this.lang = event.lang;
     });
-  }
-
-  copyEmail() {
-    navigator.clipboard.writeText(this.email);
-    this.textTooltip = "Copiado"
-    setTimeout(() => {
-      this.textTooltip = ""
-    }, 2500)
   }
 }
