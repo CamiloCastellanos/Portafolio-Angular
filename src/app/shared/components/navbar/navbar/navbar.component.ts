f97d14b2bdde0c518d839edfc9d579a4193fd264
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2025-06-03 10:01:03 -0500

    Se modifica el llamado de los tipos de navbar

--- src/app/shared/components/navbar/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar/navbar.component.ts
@@ -8,33 +8,8 @@ import { ThemeService } from '../../../core/services/theme.service';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+
   constructor(private translate: TranslateService, private themeService: ThemeService) { }
-
-  displayMenu(): void {
-    let burger = document.querySelector('.burger');
-    let menu = document.querySelector('#navBarMenu');
-    burger?.classList.toggle('is-active');
-    menu?.classList.toggle('is-active');
-
-    if (document.getElementById("overlay")?.classList.contains("overlay")) {
-      document.getElementById("overlay")?.classList.remove("overlay");
-      document.getElementById("mobileMenu")?.classList.remove("is-fixed-top");
-    } else {
-      document.getElementById("overlay")?.classList.add("overlay");
-      document.getElementById("mobileMenu")?.classList.add("is-fixed-top");
-    }
-    window.scrollTo(0, 0)
-  }
-
-  pickUpMenu(): void {
-    let burger = document.querySelector('.burger');
-    let menu = document.querySelector('#navBarMenu');
-    burger?.classList.remove('is-active');
-    menu?.classList.remove('is-active');
-    document.getElementById("overlay")?.classList.remove("overlay");
-    document.getElementById("mobileMenu")?.classList.remove("is-fixed-top");
-    window.scrollTo(0, 0)
-  }
 
   changeLanguage(idiomaSeleccionado: string) {
     this.translate.use(idiomaSeleccionado.valueOf());
