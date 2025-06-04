70de158300b9c67b9c9ac616cd4d9baf6d817882
Juan Camilo Castellanos Puentes <camilo.castellanos.puentes@gmail.com>
2025-06-03 19:34:36 -0500

    Se adiciona lógica para descarga de cv del botón lateral dependiendo del idioma.

--- src/app/shared/components/social-network/social-network/social-network.component.ts
+++ src/app/shared/components/social-network/social-network/social-network.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-social-network',
@@ -7,5 +8,13 @@ import { CommonModule } from '@angular/common';
   styleUrl: './social-network.component.scss'
 })
 export class SocialNetworkComponent {
+  lang: string = "es";
+  curriculumVitaeEn = "https://drive.google.com/file/d/1uA8rPnOOOQmZM9FJQgI1m5DXGEt_MpN5/view?usp=drive_link";
+  curriculumVitaeEs = "https://drive.google.com/file/d/1iiOjGLteQNDpTcoNLteAJIJM5gcYWjq7/view?usp=drive_link";
 
+  constructor(private translate: TranslateService) {
+    this.translate.onLangChange.subscribe((event: LangChangeEvent) => {
+      this.lang = event.lang;
+    });
+  }
 }
