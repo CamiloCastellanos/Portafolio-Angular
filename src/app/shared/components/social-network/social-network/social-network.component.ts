92c17521fa9ac33a7716d653ed4bb656cab0031c
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2025-08-27 21:37:29 -0500

    Se realiza actualización a Angular 20

--- src/app/shared/components/social-network/social-network/social-network.component.ts
+++ src/app/shared/components/social-network/social-network/social-network.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+
 import { LangChangeEvent, TranslateService } from '@ngx-translate/core';
 
 @Component({
