e1b5268e4c3bb855304f03d7ae4e97814114e0b7
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-13 21:30:07 -0500

    ajuste de environmet para netlify

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,13 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: "${process.env.FIREBASE_API_KEY || ''}",
+    authDomain: "${process.env.FIREBASE_AUTH_DOMAIN || ''}",
+    projectId: "${process.env.FIREBASE_PROJECT_ID || ''}",
+    storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET || ''}",
+    messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID || ''}",
+    appId: "${process.env.FIREBASE_APP_ID || ''}",
+    measurementId: "${process.env.FIREBASE_MEASUREMENT_ID || ''}"
   }
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: import.meta.env['apiKey'] || '',
-    authDomain: import.meta.env['authDomain'] || '',
-    databaseURL: import.meta.env['databaseURL'] || '',
-    projectId: import.meta.env['projectId'] || '',
-    storageBucket: import.meta.env['storageBucket'] || '',
-    messagingSenderId: import.meta.env['messagingSenderId'] || '',
-    appId: import.meta.env['appId'] || '',
-    measurementId: import.meta.env['measurementId'] || '',
+    apiKey: "${process.env.FIREBASE_API_KEY || ''}",
+    authDomain: "${process.env.FIREBASE_AUTH_DOMAIN || ''}",
+    projectId: "${process.env.FIREBASE_PROJECT_ID || ''}",
+    storageBucket: "${process.env.FIREBASE_STORAGE_BUCKET || ''}",
+    messagingSenderId: "${process.env.FIREBASE_MESSAGING_SENDER_ID || ''}",
+    appId: "${process.env.FIREBASE_APP_ID || ''}",
+    measurementId: "${process.env.FIREBASE_MEASUREMENT_ID || ''}"
   }
 };
