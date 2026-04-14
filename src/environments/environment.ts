d5bfc3144393e9c18ab7f0ea771ed98c273140ef
Camilo Castellanos <camilo.castellanos.puentes@gmail.com>
2026-04-13 21:24:29 -0500

    Intento de configurar netlify con el environment del proyecto

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,13 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: "",
-    measurementId: ""
+    apiKey: import.meta.env['apiKey'] || '',
+    authDomain: import.meta.env['authDomain'] || '',
+    databaseURL: import.meta.env['databaseURL'] || '',
+    projectId: import.meta.env['projectId'] || '',
+    storageBucket: import.meta.env['storageBucket'] || '',
+    messagingSenderId: import.meta.env['messagingSenderId'] || '',
+    appId: import.meta.env['appId'] || '',
+    measurementId: import.meta.env['measurementId'] || '',
   }
 };
